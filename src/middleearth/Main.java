0e7c83f10dc06cad264f8984d363d7aa018fe61c
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-03-02 13:56:59 -0600

    Second commit, testing

--- src/middleearth/Main.java
+++ src/middleearth/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("git@github.com:steinmeyerspencer/CS3330_Proj2.git");
+		System.out.println("Second try");
 	}
 
 }
