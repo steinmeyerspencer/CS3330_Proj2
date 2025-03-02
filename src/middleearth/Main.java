53b0111827704d0cb33ffdcf7a1bea27e9fe7b8a
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-03-02 13:55:19 -0600

    Please work

--- src/middleearth/Main.java
+++ src/middleearth/Main.java
@@ -3,8 +3,7 @@ package middleearth;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("git@github.com:steinmeyerspencer/CS3330_Proj2.git");
 	}
 
 }
