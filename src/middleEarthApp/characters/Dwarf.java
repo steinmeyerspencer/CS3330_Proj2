fbcf1e1b18ab4e8cc1d272afa8347c23deda1a2a
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-03-02 14:31:42 -0600

    Finished elf class. Removed Race enum because not necessary. Started on dwarf class

---
+++ src/middleEarthApp/characters/Dwarf.java
@@ -0,0 +1,5 @@
+package middleEarthApp.characters;
+
+public class Dwarf {
+
+}
--- src/middleEarthApp/characters/Elf.java
+++ src/middleEarthApp/characters/Elf.java
@@ -3,11 +3,23 @@ package middleEarthApp.characters;
 public class Elf extends MiddleEarthCharacter{
 
 	private String race = "Elf";
-	
+		
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
-		// TODO Auto-generated method stub
+		if (target.getRace() == "Orc") {
+			target.setHealth(target.getHealth() - (1.5 * this.power));
+			return true;
+		}
+		else if (target.getRace() == "Dwarf" || target.getRace() == "Elf") {
+			
+			return false;
+		}
+		else if (target.getRace() == "Human" || target.getRace() == "Wizard") {
+			target.setHealth(target.getHealth() - this.power);
+			return true;
+		}
 		return false;
+		
 	}
 
 	@Override
