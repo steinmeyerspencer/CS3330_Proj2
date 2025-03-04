c59b9ce99aa662b0cc8f82ae8d7a43e0f0c53947
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-03-03 21:18:03 -0600

    Made functions in Menu that get integers or strings from an input. Will use for all functions required in Main to cut down on code bulk

--- src/middleEarthApp/Menu.java
+++ src/middleEarthApp/Menu.java
@@ -4,22 +4,39 @@ import java.util.Scanner;
 
 public class Menu {
 	
+	
+	/**
+	 * constructor so I can use menu class
+	 */
+	public Menu() {
+		
+	}
+
+	// starts scanner
 	Scanner input = new Scanner(System.in);
 	
-	public int printMenu() {
-		
-		System.out.println("1. Add a new character.");
-		System.out.println("2. View all characters.");
-		System.out.println("3. Update a character.");
-		System.out.println("4. Delete a character.");
-		System.out.println("5. Execute all characters' attack actions.");
-		System.out.println("6. Exit.");
+	/**
+	 * parameterized function getting integer from an input
+	 * returns integer if is in range of [startInclusive, endInclusive]
+	 * loops until integer in range is submitted
+	 * @param startInclusive
+	 * @param endInclusive
+	 * @return
+	 */
+	private int getIntFromInput(int startInclusive, int endInclusive) {
 		while(true) {
 			if(input.hasNextInt()) {
-				int answer = input.nextInt();
-				System.out.println("You entered " + answer);
-				input.nextLine();
-				return answer;
+				if(input.nextInt() >= startInclusive || input.nextInt() <= endInclusive) {
+					int answer = input.nextInt();
+					System.out.println("You entered " + answer);
+					input.nextLine();
+					return answer;
+				}
+				else {
+					System.out.println("Integer not in accepted range");
+					input.nextLine();
+					continue;
+				}
 			}
 			else {
 				System.out.println("Input not recognized as an integer, try again.");
@@ -29,39 +46,66 @@ public class Menu {
 		}
 	}
 	
-	public boolean runAdd() {
+	/**
+	 * nonparameterized function getting a string from input
+	 * returns string if input is a string
+	 * @return
+	 */
+	private String getStringFromInput() {
 		while(true) {
-			System.out.println("Enter Name: ");
-			if(input.hasNext()) {
-				String name = input.next();
+			if(input.next() instanceof String) {
+				String string = input.next();
 				input.nextLine();
+				return string;
 			}
 			else {
-				System.out.println("Name not recognized, try again.");
+				System.out.println("Input not recognized as a string, try again.");
 				input.nextLine();
 				continue;
 			}
-			System.out.println("Enter health: ");
-			if(input.hasNextInt()) {
-				int health = input.nextInt();
-				input.nextLine();
-			}
-			else {
-				System.out.println("Input not recognized as an integer, try again.");
-				input.nextLine();
-				continue;
-			}
-			System.out.println("Enter power: ");
-			if(input.hasNextInt()) {
-				int power = input.nextInt();
-				input.nextLine();
-			}
-			else {
-				System.out.println("Input not recognized as an integer, try again.");
-				input.nextLine();
-				continue;
-			}
+		}
 	}
+	
+	
+	
+	/**
+	 * prints menu and returns selected choice
+	 * @return
+	 */
+	public int printMenu() {
+		System.out.println("1. Add a new character.");
+		System.out.println("2. View all characters.");
+		System.out.println("3. Update a character.");
+		System.out.println("4. Delete a character.");
+		System.out.println("5. Execute all characters' attack actions.");
+		System.out.println("6. Exit.");
+		return getIntFromInput(1,6);
+	}
+
+	
+	
+	public boolean runAdd() {
+		
+		System.out.println("Enter Name: ");
+		String name = getStringFromInput();
+		
+		System.out.println("Enter health (1-1000): ");
+		int health = getIntFromInput(1,1000);
+		
+		System.out.println("Enter power (1-100): ");
+		int power = getIntFromInput(1,100);
+		
+		System.out.println("What race would you like your character to be?");
+		System.out.println("1. Dwarf");
+		System.out.println("2. Elf");
+		System.out.println("3. Human");
+		System.out.println("4. Orc");
+		System.out.println("5. Wizard");
+		int race = getIntFromInput(1,5);
+		return true;
+	
 	}
 	
 }
+
+
--- src/middleEarthApp/MiddleEarthApp.java
+++ src/middleEarthApp/MiddleEarthApp.java
@@ -1,11 +1,11 @@
 package middleEarthApp;
 
-import java.util.Scanner;
 
 public class MiddleEarthApp {
 
 	public static void main(String[] args) {
-		
+		Menu menu = new Menu();
+		menu.printMenu();
 
 		
 	}
