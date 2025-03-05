ead2ff355931018d9695a1c8f8d4f5bf10cbaadb
MelanieBishop <MelanieBishop@localhost>
2025-03-04 18:25:50 -0600

    Error checked and updated a few areas of the program to handle errors.

--- src/middleEarthApp/charactermanager/CharacterManager.java
+++ src/middleEarthApp/charactermanager/CharacterManager.java
@@ -4,24 +4,28 @@ import middleEarthApp.characters.MiddleEarthCharacter;
 
 public class CharacterManager {
 	
+	
 	/**
 	 * creating attributes
 	 * Made by Dane
 	 */
 	public MiddleEarthCharacter[] characters;
-	public int size = 0;
+	public int size;
 	
 	
-//	/**
-//	 * non parameterized constructor
-//	 * with array size starting at 5
-//	 * Made by Dane
-//	 */
-//	public CharacterManager() {
-//		this.characters = new MiddleEarthCharacter[5];
-//		this.size = 0;
-//		
-//	}
+	
+	/**
+	 * non parameterized constructor
+	 * with array size starting at 5
+	 * Made by Dane
+	 */
+	public CharacterManager() {
+		
+		this.characters = new MiddleEarthCharacter[5];
+		this.size = 0;
+		
+	}
+	
 	
 	/**
 	 * creating method to add character to Character array
@@ -33,29 +37,48 @@ public class CharacterManager {
 	 * @return
 	 */
 	public boolean addCharacter(MiddleEarthCharacter c) {
+		
+
 		// Doubles the array if the array is full
 		if (size == characters.length) {
 			MiddleEarthCharacter[] newCharacters = new MiddleEarthCharacter[characters.length * 2];
+			
+			
 			for(int i = 0; i < characters.length; i++) {
+				
 				newCharacters[i] = characters[i];
+				
 			}
+			
 			characters = newCharacters;
+			
+			
 		}
-
+		
+		
+		
+		
 		// add character
 		for(int i = 0; i < characters.length; i++) {
+			
 			if (characters[i] == null) {
+				
 				characters[i] = c;
 				size++;
 				System.out.println("Character added");
-				c.displayInfo();
-				System.out.println("and they are a " + c.getRace());
 				return true;
+				
 			}	
+			
 		}
 		System.out.println("ERROR: Something went wrong.");
 		return false;	
 	}
+	
+	
+	
+	
+	
 	
 	/**
 	 * Method to get a MiddleEarthCharacter by name
@@ -69,13 +92,17 @@ public class CharacterManager {
 		// get the character via the input name
 		for (int i = 0; i < characters.length; i++) {
 			if (name.equals(characters[i].getName())) {
-				System.out.println("Character with name " + name + " retrieved!");
+				// System.out.println("Character" + name + "retrieved!");
 				return characters[i];
 			}
 		}
-		System.out.println(name + " does not exist");
+		
+		System.out.println(name + "does not exist");
 		return null;
+
 	}
+	
+	
 	
 	
 	/**
@@ -88,30 +115,30 @@ public class CharacterManager {
 	 * @return
 	 */
 	public boolean updateCharacter (MiddleEarthCharacter character, String name, int health, int power) {
+		
 		// Find the character in the current characters list
 		for (int i = 0; i < characters.length; i++) {
 			if (character == characters[i]) {
 				// update character attributes
-				try {
-					characters[i].setName(name);
-					characters[i].setHealth(health);
-					characters[i].setPower(power);
-				}
-				catch(Exception e) {
-					System.out.println("Error with update: " + e);
-				}
+				characters[i].name = name;
+				characters[i].health = health;
+				characters[i].power = power;
 				
 				System.out.println("Attributes updated.");
 				return true;
 			}
+			
 		}
+		
 		System.out.println("ERROR: Could not find character.");
 		return false;
+		
 	}
+	
+	
 	
 	/**
 	 * Method that deletes a character based on the passed character argument
-	 * Then reindexes so that the array's first "size" elements are full and the rest are null
 	 * Made by Dane
 	 * @param character
 	 * @return
@@ -119,16 +146,16 @@ public class CharacterManager {
 	public boolean deleteCharacter(MiddleEarthCharacter character) {
 		for (int i = 0; i < characters.length; i++) {
 			if (character == characters[i]) {
+				
 				for (int j = i; j < characters.length - 1; j++) {
 					characters[j] = characters[j + 1];
 				}
-				// should it be j-1 if you're shifting all of the elements toward the beginning?
-				// should you delete the character first?
-				// need to change "size" to size--
 				characters[characters.length - 1] = null;
+				size--;
 				System.out.println("Character deleted.");
 				return true;
 			}
+			
 		}
 		System.out.println("ERROR: Could not find character.");
 		return false;
@@ -141,10 +168,27 @@ public class CharacterManager {
 	 * Made by Dane
 	 */
 	public void displayAllCharacters() {
+		
+		int emptySpaces = 0;
 		for (int i = 0; i < characters.length; i++) {
-			characters[i].displayInfo();
+			
+			if (characters[i] == null) {
+				emptySpaces++;
+				continue;
+			}else {
+				System.out.println("");
+				characters[i].displayInfo();
+			}
+			
+			
 		}
+		
+		if (emptySpaces == characters.length) {
+			System.out.println("\nThere are no characters yet. Enter option 1 to add a new character.\n");
+		}
+		
 	}
+	
 	
 	/**
 	 * executeAllAttacks that is the 5ht option in the menu
